c9b64f37a1bb4dc2fefc8e7fe3f1636f895badbd
Michael Lowry <mjl9ka@virginia.edu>
2025-09-30 14:47:15 -0400

    ndcrescent.cpp can estimate volume between two d spheres

--- ndcrescent.cpp
+++ ndcrescent.cpp
@@ -11,9 +11,9 @@ void print_vec(int dim, double vec[]) {
 }
 
 void scale_vec(int dim, double* vec, double scale) {
-    // scale a vector
+    // scale a positive vector to have components between -scale/2 and scale/2
     for (int i = 0; i<dim; ++i) {
-        *(vec+i) = *(vec+i) * scale;
+        *(vec+i) = *(vec+i) * scale - (scale/2);
     }
 }
 
@@ -26,6 +26,53 @@ double vector_mag(int dim, double vec[]) {
     double mag = std::pow(sum, 0.5);
     return mag;   
 
+}
+
+bool check_inside(int dim, double vec[], double r, double offset) {
+    // check if vector is inside a dim dimensional sphere offset from the origin in the first dimension
+    vec[0] = vec[0] - offset;
+    double offset_mag = vector_mag(dim, vec);
+    vec[0] = vec[0] + offset;
+    return offset_mag <= r;
+}
+
+double* monte_volume(int d, int N, double r1, double r2, double a) {
+    QuasiRandomSobol srand(d); // sobol quasi random number generator in d dimensions
+    double *d_rand = new double[d]; // double array to hold random vector in d dimensions
+    double box_length = r1+r2+a; // length of box holding the two spheres
+    std::cout<<"Box Length: "<<box_length<<std::endl;
+    double box_volume = std::pow(box_length, d); // volume of the box
+
+    int num_inside = 0;
+
+    for (int i=0; i<N; ++i) {
+        srand.RndmArray(d, d_rand); // generate a random vector
+        // print_vec(d, d_rand);
+        scale_vec(d, d_rand, box_length); // scale the vector by the box length
+        // print_vec(d, d_rand);
+        double d_rand_mag = vector_mag(d, d_rand); // calculate the magnitude of the vector
+        if (check_inside(d, d_rand, r1, a/2) && check_inside(d, d_rand, r2, -a/2)) {
+            // std::cout<<"Inside"<<std::endl;
+            num_inside++;
+        }
+        // print_vec(d, d_rand);
+        // std::cout<<d_rand_mag<<std::endl;
+    }
+
+    std::cout<<"n_inside: "<<num_inside<<std::endl;
+    double p = num_inside/(N*1.);
+    std::cout<<"p: "<<p<<std::endl;
+    double volume = p*box_volume;
+    std::cout<<"Volume: "<<volume<<std::endl;
+    double stdev = 0;
+
+    double* results = new double[3];
+    results[0] = p;
+    results[1] = volume;
+    results[2] = stdev;
+
+    delete[] d_rand;
+    return results;
 }
 
 int main(int argc, char* argv[]) {
@@ -43,24 +90,10 @@ int main(int argc, char* argv[]) {
     double a = std::atof(argv[5]); // offset of spheres
     
     // ******* add your code here 
-    QuasiRandomSobol srand(d); // sobol quasi random number generator in d dimensions
-    double d_rand[d]; // double array to hold random vector in d dimensions
-    double box_length = r1+r2+a; // length of box holding the two spheres
-    std::cout<<"Box Length: "<<box_length<<std::endl;
-    double box_volume = std::pow(box_length, d); // volume of the box
 
-
-    for (int i=0; i<N; ++i) {
-        srand.RndmArray(2, d_rand); // generate a random vector
-        print_vec(d, d_rand);
-        scale_vec(d, d_rand, box_length); // scale the vector by the box length
-        print_vec(d, d_rand);
-        double d_rand_mag = vector_mag(d, d_rand); // calculate the magnitude of the vector
-        std::cout<<d_rand_mag<<std::endl;
-    }
-
-    double volume = 0;
-    double stdev = 0;
+    double* results = monte_volume(d, N, r1, r2, a);
+    double volume = results[1];
+    double stdev = results[2];
 
     // *******
 
@@ -70,5 +103,6 @@ int main(int argc, char* argv[]) {
     std::cout << "volume: " << volume << std::endl;
     std::cout << "stat uncertainty: " << stdev << std::endl;
 
+    delete[] results;
     return 0; // Exit successfully
 }
--- ndcrescent.h
+++ ndcrescent.h
@@ -11,3 +11,17 @@
 #include "TStyle.h"
 #include <cmath>
 
+// print a dim dimensional vector
+void print_vec(int dim, double vec[]);
+
+// scale double vector in place
+void scale_vec(int dim, double* vec, double scale);
+
+// calculate the magnitude of a double vector
+double vector_mag(int dim, double vec[]);
+
+// check if a vector defined off the origin is inside a sphere offset from the origin
+bool check_inside(int dim, double vec[], double r, double offset);
+
+// do monte carlo integration for two d-spheres - return p, the final volume, and std dev
+double* monte_volume(int d, int N, double r1, double r2, double a);
